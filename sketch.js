962f4f1fb7cafc6e26539d278193f3962661599a
tanishirathi <72450300+tanishirathi@users.noreply.github.com>
2020-11-11 22:23:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+var groundleft,groundright,groundbottom
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+    engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+    World.add(world, ground);
+	
+	b1=new Box (400,650,100,10);
+	b2=new Box(450,605,10,100);
+    b3=new Box(350,605,10,100);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0)
+
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  
+  b1.display();
+  b2.display();
+  b3.display();
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === LEFT_ARROW) {
+  
+	  helicopterSprite.x=helicopterSprite.x-20;    
+	  translation={x:-20,y:0}
+	  Matter.Body.translate(packageBody, translation)
+  
+  
+	} else if (keyCode === RIGHT_ARROW) {
+	  helicopterSprite.x=helicopterSprite.x+20;
+	 translation={x:20,y:0}
+	  Matter.Body.translate(packageBody, translation)
+	}
+	else if (keyCode === DOWN_ARROW) {
+	  Matter.Body.setStatic(packageBody,false);
+	  
+	}
+  }
